bf41d22a212967f75906940b3e51d940b5db6e21
Dakh-o <danielluciano509@aragon.unam.mx>
2025-11-05 20:41:48 -0600

    Bala Audio
    Se agrego el sonido a la bala cada vez que se dispara

--- Assets/Scripts/Disparo.cs
+++ Assets/Scripts/Disparo.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Disparo : MonoBehaviour
-
 {
     [SerializeField] private Transform controladorDisparo;
-
     [SerializeField] private GameObject bala;
 
-    // private void Update()
-    //{
-    //  if (Input.GetButtonDown("Fire1"))
-    //{
-    // Disparar();
-    //}
-    //}
+    [SerializeField] private AudioSource audioSource;   // <-- nuevo
+    [SerializeField] private AudioClip sfxDisparo;      // <-- nuevo
+
     private void Disparar()
     {
         Instantiate(bala, controladorDisparo.position, controladorDisparo.rotation);
+
+        // reproducir sonido
+        if (audioSource != null && sfxDisparo != null)
+        {
+            audioSource.PlayOneShot(sfxDisparo);
+        }
     }
 }
-
